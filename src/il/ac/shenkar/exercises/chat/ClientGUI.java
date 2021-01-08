8028f00de6c13ef00085dad3aaaec197644ce16c
Danityec <Danityec@gmail.com>
2021-01-08 22:08:44 +0200

    update

--- src/il/ac/shenkar/exercises/chat/ClientGUI.java
+++ src/il/ac/shenkar/exercises/chat/ClientGUI.java
@@ -9,6 +9,8 @@ import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.net.Socket;
 
+
+
 public class ClientGUI implements StringConsumer, StringProducer{
 
     //GUI components signUp
@@ -17,6 +19,12 @@ public class ClientGUI implements StringConsumer, StringProducer{
     private JPanel chatPanel;
     private JTextField clientInput;
     private String name ="";
+    private JLabel title;
+    private JPanel panelBottom, panelTop, contentPanel;
+    private JTextArea messageBoard;
+    private JLabel headSignUp;
+
+
 
     private StringConsumer consumer;
 
@@ -27,11 +35,58 @@ public class ClientGUI implements StringConsumer, StringProducer{
             public void windowClosing(WindowEvent event) {
                 chatApp.setVisible(false);
                 chatApp.dispose();
-                ConnectionProxy.consume("disconnect");
-                closeConnection(socket);
+
                 System.exit(0);
             }
         });
+        panelBottom = new JPanel();
+        panelTop = new JPanel();
+        contentPanel = new JPanel();
+        title = new JLabel();
+
+        Container container = chatApp.getContentPane();
+
+        panelBottom.setBackground(Color.gray);
+        panelTop.setBackground(Color.lightGray);
+        contentPanel.setBackground(Color.lightGray);
+
+        panelBottom.setLayout(new BorderLayout());
+        panelTop.setLayout(new FlowLayout());
+        contentPanel.setLayout(new BorderLayout());
+        container.setLayout(new BorderLayout());
+
+        panelBottom.add("North", title);
+        panelBottom.add(contentPanel);
+        container.add("North",panelTop);
+        container.add("Center",panelBottom);
+
+        ActionListener homeAction = new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                homePage();
+            }
+        };
+    }
+
+    public void start() {
+        chatApp.setVisible(true);
+    }
+
+    public void homePage() {
+        title.setText("Chat App");
+        contentPanel.removeAll();
+        chatApp.setSize(500, 150);
+
+        JPanel chatArea = new JPanel();
+        JLabel descriptionName = new JLabel("Name");
+        JTextField descriptionTxt = new JTextField();
+
+        JButton submitBtn = new JButton("Add to chat");
+
+        chatArea.add(descriptionName);
+        chatArea.add(descriptionTxt);
+        chatArea.add(submitBtn);
+
+
     }
 
 
@@ -40,51 +95,59 @@ public class ClientGUI implements StringConsumer, StringProducer{
 
     // set the UI of sign-up
     public void signUp() {
-        //set background color to panel1 and panel 2
-        panel1.setBackground(Color.PINK);
-        panel2.setBackground(Color.PINK);
 
-        frameSignUp.setLayout(new FlowLayout());
+        //set background color to panel1 and panel 2
+        chatPanel.setBackground(Color.PINK);
+        //panel2.setBackground(Color.PINK);
+
+        chatApp.setLayout(new FlowLayout());
 
         //set the fonts in signup frame
-        headSignUp.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
+        chatApp.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
         submitButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
         submitButton.setBackground(Color.magenta);
 
         //add the text box,button and header to the panels
-        panel1.add(headSignUp);
-        panel2.add(clientDeatils);
-        panel2.add(submitButton);
+        chatPanel.add(headSignUp);
+        //panel2.add(clientInput);
+        //panel2.add(submitButton);
 
         //add the panels to the frame
-        frameSignUp.add(panel1);
-        frameSignUp.add(panel2);
+        chatApp.add(chatPanel);
+        //frameSignUp.add(panel2);
 
         //set the size of the frame(window)
-        frameSignUp.setSize(350, 200);
+        chatApp.setSize(350, 200);
 
         //set the sign-up window to be visible
-        frameSignUp.setVisible(true);
+        chatApp.setVisible(true);
 
     }
 
-    //set the UI of the chat
     public void chat() {
-        //set the background colors
+
+        JPanel displayHead =   new JPanel();
+        JPanel writeText =   new JPanel();
+        JPanel displayText =   new JPanel();
+        JButton sendButton =   new JButton();
+        JLabel headChat =   new JLabel();
+        JFrame frameChat = new JFrame();
+        JTextField message = new JTextField();
+
         displayText.setBackground(Color.PINK);
         writeText.setBackground(Color.PINK);
         sendButton.setBackground(Color.magenta);
 
         //set the fonts
-        sendButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
-        headChat.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
+        //sendButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+        //headChat.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
 
         frameChat.setLayout(new FlowLayout());
 
         //add the text box,button and header to the places in the window
         displayHead.add(headChat);
         displayText.add(messageBoard);
-        writeText.add(textMessage);
+        writeText.add(message);
         writeText.add(sendButton);
 
         //add all panels(write and display) to the frame
@@ -106,24 +169,29 @@ public class ClientGUI implements StringConsumer, StringProducer{
         submitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //save the nickname name the user sent
-                nickname = clientDeatils.getText();
+                name = clientInput.getText();
                 //start the chat
                 runChat();
             }
         });
     }
+
     //run the chat
     public void runChat() {
         try {
+            JButton sendButton =   new JButton();
+            JTextField message = new JTextField();
+            JFrame frameChat = new JFrame();
+
             //close sign-up window
-            closeSignUp();
+            //closeSignUp();
             //display the chat window
             chat();
             //open new socket and connect the user to this port
             Socket socket = new Socket("127.0.0.1",1400);
             ConnectionProxy connectionProxy = new ConnectionProxy(socket);
             connectionProxy.addConsumer(this);
-            connectionProxy.consume(nickname);
+            connectionProxy.consume(name);
             //start connectionProxy
             connectionProxy.start();
 
@@ -131,11 +199,11 @@ public class ClientGUI implements StringConsumer, StringProducer{
             sendButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     //display the message to the window
-                    String message = textMessage.getText();
+                    String msg = message.getText();
                     try {
                         //put the name of the sender before the message
-                        connectionProxy.consume(message);
-                        textMessage.setText("");
+                        connectionProxy.consume(msg);
+                        message.setText("");
 
                     } catch (IOException ioException) {
                         ioException.printStackTrace();
@@ -145,17 +213,16 @@ public class ClientGUI implements StringConsumer, StringProducer{
 
             //Register the listener for the form using the anonymous inner class
             frameChat.addWindowListener(new WindowAdapter() {
-                @Override
                 public void windowClosing(WindowEvent e) {
-                    try {
-                        //when the user disconnect
+                    //when the user disconnect
 
-
-                    } catch (IOException ioException) {}
 
                     super.windowClosing(e);
                     //close the window
-                    closeChat();
+                    //closeChat();
+                    headSignUp = null;
+                    clientInput = null;
+
                     //close the socket
                     closeConnection(socket);
                     System.exit(0);
